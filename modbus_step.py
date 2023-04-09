1427c59f85e7cfa155b066c6d710cbaebaec9b78
İkranur Güler <62421679+gulerikra@users.noreply.github.com>
2023-04-09 14:07:10 +0300

    Update modbus_step.py

--- modbus_step.py
+++ modbus_step.py
@@ -1,15 +1,12 @@
 import time
 from pyModbusTCP.client import ModbusClient
 
-
 server_host="192.168.3.250"
 server_port=502
-
 
 plc=ModbusClient()
 plc.host(server_host)
 plc.port(server_port)
-
 
 while True:
     if not plc.is_open(): 
@@ -17,8 +14,6 @@ while True:
             print("unable to connect to"+server_host+":"+str(server_port))
             time.sleep(2)
 
-
     if plc.is_open():
         plc.write_multiple_coils(5,[1])
         # plc.write_multiple_coils(6,[1])
-
